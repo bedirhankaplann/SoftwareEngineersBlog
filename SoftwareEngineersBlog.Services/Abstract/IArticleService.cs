7b6a09733b9838115de6c19bd732f8466f055aa4
Bedirhan Kaplan <bedirhankaplan@outlook.com.tr>
2021-11-09 17:39:14 +0300

    Added ArticleDto and IArticleService

---
+++ SoftwareEngineersBlog.Services/Abstract/IArticleService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftwareEngineersBlog.Entities.Concrete;
+using SoftwareEngineersBlog.Entities.Dtos;
+using SoftwareEngineersBlog.Shared.Utilities.Results.Abstract;
+
+namespace SoftwareEngineersBlog.Services.Abstract
+{
+    public interface IArticleService
+    {
+        Task<IDataResult<ArticleDto>> Get(int articleId);
+        Task<IDataResult<ArticleListDto>> GetAll();
+        Task<IDataResult<ArticleListDto>> GetAllNonDeleted();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive();
+        Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId);
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createByName);
+        Task<IResult> Update(ArticleUpdateDto articleAddDtoUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId, string modifiedByName);
+
+        Task<IResult> HardDelete(int articleId);
+    }
+}
