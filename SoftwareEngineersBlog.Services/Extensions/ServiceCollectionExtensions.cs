831def4ee5b4517ec3f59a533bc9233f7d11597c
Bedirhan Kaplan <bedirhankaplan@outlook.com.tr>
2021-11-15 15:20:45 +0300

    Updated Startup Added MVC and Area

---
+++ SoftwareEngineersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using SoftwareEngineersBlog.Data.Abstract;
+using SoftwareEngineersBlog.Data.Concrete;
+using SoftwareEngineersBlog.Data.Concrete.EntityFramework.Contexts;
+using SoftwareEngineersBlog.Services.Abstract;
+using SoftwareEngineersBlog.Services.Concrete;
+
+namespace SoftwareEngineersBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<SoftwareEngineersBlogContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+
+            return serviceCollection;
+        }
+    }
+}
