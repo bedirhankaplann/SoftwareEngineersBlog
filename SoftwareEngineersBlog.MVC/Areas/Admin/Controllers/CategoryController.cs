c6ae91637cabd1d80960732771310a6342fdde94
Bedirhan Kaplan <bedirhankaplan@outlook.com.tr>
2021-11-19 17:48:43 +0300

    Added Category/Index and DataTable

---
+++ SoftwareEngineersBlog.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SoftwareEngineersBlog.Services.Abstract;
+using SoftwareEngineersBlog.Shared.Utilities.Results.ComplexTypes;
+
+namespace SoftwareEngineersBlog.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
